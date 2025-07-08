720f9f62e534c3fdc4369aa425925d3322716074
Tomi Javanainen <tomi.javanainen@proton.me>
2025-07-09 00:08:28 +0300

    Add nanorc

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(realpath $0)
-SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+function append_to_file_if_not_there {
+  grep "${1}" "${2}" > /dev/null || echo "${1}" >> "${2}"
+}
 
-# Set repo path for updater in fish config
+SCRIPT_DIR=$(dirname $(realpath $0))
+
+echo "Set KONSO_SETTINGS_* env vars"
 fish -c "set -Ux KONSO_SETTINGS_REPO ${SCRIPT_DIR}"
 fish -c "set -Ux KONSO_SETTINGS_LAST_UPDATE $(date +\"%s\")"
 
 echo "Install fish config file"
 mkdir -p -v "${HOME}/.config/fish"
 FISH_CONFIG="${HOME}/.config/fish/config.fish"
-# Remove duplicates
-# TODO: trim blank lines
-sed -i "s;# Shared fish config;;g" "${FISH_CONFIG}"
-sed -i "s;. ${SCRIPT_DIR}/config.fish;;g" "${FISH_CONFIG}"
-echo "" >> "${FISH_CONFIG}"
-echo "# Shared fish config" >> "${FISH_CONFIG}"
-echo ". ${SCRIPT_DIR}/config.fish" >> "${FISH_CONFIG}"
+append_to_file_if_not_there "# Shared fish config" "${FISH_CONFIG}"
+append_to_file_if_not_there ". ${SCRIPT_DIR}/config.fish" "${FISH_CONFIG}"
+
+echo "Install nanorc"
+NANORC_DIR="${HOME}/.config/nano"
+NANORC="${NANORC_DIR=}/nanorc"
+mkdir -p -v "${NANORC_DIR}"
+if [[ -f "${NANORC}" ]] \
+ && [[ $(readlink -f "${NANORC}") != "${SCRIPT_DIR}/nanorc" ]]
+then
+  echo "${NANORC} exists"
+  mv -v --backup=numbered "${NANORC}" "${NANORC}.konso.bak"
+fi
+echo "Create nanorc symlink"
+ln -svf "${SCRIPT_DIR}/nanorc" "${NANORC}"
+
