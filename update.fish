d85a26bfe5d346077a8d9cb9e93114c83296b8c2
Tomi Javanainen <tomi.javanainen@proton.me>
2025-07-09 00:26:54 +0300

    Re-run install script if settings were updated

--- update.fish
+++ update.fish
@@ -1,14 +1,26 @@
 #!/usr/bin/env fish
-
 # SELF UPDATE
+
 # Check updates once a day
 set update_interval 86400
 set current_timestamp (date +"%s")
-if test $current_timestamp -ge \
+
+if ! test $current_timestamp -ge \
    (math $KONSO_SETTINGS_LAST_UPDATE + $update_interval)
-  echo "Update shared settings..."
-  fish -c "cd $KONSO_SETTINGS_REPO && git pull"
-  set -Ux KONSO_SETTINGS_LAST_UPDATE $current_timestamp
-else
   echo "Last settings update was at $(date --date @$KONSO_SETTINGS_LAST_UPDATE --rfc-3339=seconds), no need to update"
+  exit 0
 end
+
+echo "Update shared settings..."
+set saved_pwd (pwd)
+set git_pull_result (cd $KONSO_SETTINGS_REPO && git pull)
+cd $saved_pwd
+set -Ux KONSO_SETTINGS_LAST_UPDATE $current_timestamp
+
+if test $git_pull_result = "Already up to date."
+  echo $git_pull_result
+  exit 0
+end
+
+echo "Re-run install.sh..."
+$KONSO_SETTINGS_REPO/install.sh
