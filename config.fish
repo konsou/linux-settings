b524ea3a3e4e5cd6df5e5e57853690bdb972e30d
Tomi Javanainen <tomi.javanainen@proton.me>
2025-07-20 15:58:57 +0300

    Add convenience stuff from Cachyos

--- config.fish
+++ config.fish
@@ -4,17 +4,38 @@ if status is-interactive
   # FISH SETTINGS
   # Disable the "Welcome to fish..." text
   set -Ux fish_greeting ""
+  function fish_greeting
+    # pass
+  end
+  # Format man pages
+  set -x MANROFFOPT "-c"
+  set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
 
   # ALIASES
   alias .. "cd .."
   alias anacrontab "nano ~/.anacron/etc/anacrontab"
   alias cd.. "cd .."
   alias fish_config_file "nano ~/.config/fish/config.fish"
-  alias ls "ls --time-style=iso --color -lh"
+
+  # Replace ls with eza
+  alias ls='eza -l --time-style=iso --color=always --group-directories-first --icons'  # preferred listing
+  alias la='eza -a --time-style=iso --color=always --group-directories-first --icons'  # all files and dirs
+  alias lt='eza -aT --time-style=iso --color=always --group-directories-first --icons' # tree listing
+  alias l.="eza -a | grep -e '^\.'"                                                    # show only dotfile
+
   alias process-list "ps aux"
   alias rtfm tldr
   alias untar "tar -xvf"
   alias upgrade-pip "python -m pip install --upgrade pip"
+
+  # Add color
+  alias dir='dir --color=auto'
+  alias vdir='vdir --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
+
 
   # SELF UPDATE
   $KONSO_SETTINGS_REPO/update.fish
