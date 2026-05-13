05a5ad65c9e6b2c3003bf1659aac5fc423cdceec
Tomi Javanainen <tomi.javanainen@proton.me>
2026-05-13 15:47:01 +0300

    Add fish plugins, install rust only if it doesn't exist, add cargo-update

--- install-utils.sh
+++ install-utils.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
-APT_INSTALL_PKGS="bat fish nala eza bashtop htop iotop jdupes jnettop ncdu nvtop tldr zoxide micro"
+APT_INSTALL_PKGS="bat fish nala eza bashtop htop iotop jdupes jnettop ncdu nvtop tldr zoxide fzf micro"
+# Needed for cargo-update
+APT_INSTALL_PKGS="${APT_INSTALL_PKGS} libssl-dev pkg-config"
 echo "Install base utils"
 echo $APT_INSTALL_PKGS
 sudo apt install ${APT_INSTALL_PKGS}
 
-echo "Install rust + cargo"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "Install fish plugins"
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+# Better tab autocomplete for zoxide
+fish -c "fisher install icezyclon/zoxide.fish"
+
+# Rust + cargo
+if [[ -f $(which rustc) ]] && [[ -f $(which cargo) ]]; then
+	echo "Rust + cargo already installed"
+else
+	echo "Install rust + cargo"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+if [[ $(cargo install --list | grep cargo-update) ]]; then
+  echo "cargo-update already installed"
+else
+	echo "Install cargo-update"
+	cargo install cargo-update
+fi
 
 echo "Install dysk"
 source "$HOME/.cargo/env"
